d15aa9fb761d43aa2a7cdacaf6f69d036e9d1986
Matheus de Holanda <31321889+matheusholannda@users.noreply.github.com>
2021-05-17 21:36:59 -0300

    Create index.php
    Introduzindo condicionais no projeto PHP

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+
+//print_r($categorias);
+
+$nome = 'Eduardo';
+$idade = '18';
+
+//var_dump($nome);
+//var_dump($idade);
+
+if ($idade >= 6 && <= 12) {
+    for ($i=0; $i <= count($categorias) ; $i++) { 
+        if ($categorias[$i] == 'infantil') {
+            echo "O nadador " .$nome. " compete na categoria infantil.";
+        }
+    }
+} elseif ($idade >=13 && <= 18) {
+    for ($i=0; $i <= count($categorias) ; $i++) { 
+        if ($categorias[$i] == 'adolescente') {
+            echo "O nadador " .$nome. " compete na categoria adolescente.";
+        }
+    }
+} else {
+    for ($i=0; $i <= count($categorias) ; $i++) { 
+        if ($categorias[$i] == 'adulto') {
+            echo "O nadador " .$nome. " compete na categoria adulto.";
+        }
+    }
+}
